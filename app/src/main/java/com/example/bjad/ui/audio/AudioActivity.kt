70a4886a4d1a8ddaa652267224499927bcf659b0
Mohit Singh <singhmohit9718@gmail.com>
2023-03-17 20:48:15 +0530

    First screen is come into the audio

--- app/src/main/java/com/example/bjad/ui/audio/AudioActivity.kt
+++ app/src/main/java/com/example/bjad/ui/audio/AudioActivity.kt
@@ -2,11 +2,19 @@ package com.example.bjad.ui.audio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.example.bjad.R
 
 class AudioActivity : AppCompatActivity() {
+    private lateinit var audioNavController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_audio)
+
+        val audioNavHost = supportFragmentManager
+            .findFragmentById(R.id.audioFragmentContainerView) as NavHostFragment
+        audioNavController =audioNavHost.findNavController()
     }
 }
